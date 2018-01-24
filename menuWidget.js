843e3ee309fe212a3ce0696f58c508117b932616
Sasha <sasha.gurys@gmail.com>
2018-01-24 17:21:57 +0300

    minor upgrades

--- menuWidget.js
+++ menuWidget.js
@@ -1,38 +1,56 @@
+// конструктор виджета
 function ContextMenu(switchButton) {
     this._$switchButton = switchButton;
+    // находим меню внутри кнопки
     this._$menu = switchButton.find('div[data-role="menu"]');
+}
 
+// добавляем методы в прототип
+ContextMenu.prototype = {
+    // при нажатии на кнопку либо прячем, либо показываем меню
+    _onSwitchButtonClick: function () {
 
-
-    this._onSwitchButtonClick = function () {
-
-        if (this._$menu.hasClass('hidden')) {
-
-            this._$menu.removeClass('hidden');
-
-            var buttonOffset = $('button[data-role="show-menu"]')[0].getBoundingClientRect().top;
-            var menuHeight = this._$menu.height() + 30;
-
-            if (buttonOffset < menuHeight) {
-                this._$menu.addClass('go-down');
-                this._$menu.css('bottom', -menuHeight);
-            }
-
+        if (this._$menu.hasClass('hidden')) { // если она спрятана, то надо показать
+            this._showMenu();
         } else {
-            this._$menu.addClass('hidden');
+            this.__hideMenu();
         }
 
 
-    };
+    },
 
-    this.init = function () {
+    _showMenu: function () {
+        
+        this._$menu.removeClass('hidden');
+        // для проверки положения кнопки относительно верха окна создаем две переменные
+        var buttonOffset = $('button[data-role="show-menu"]')[0].getBoundingClientRect().top;
+        var menuHeight = this._$menu.height() + 30;
+
+        // если кнопка слишком высоко, мы покажем меню снизу кнопки
+        if (buttonOffset < menuHeight) {
+            this._$menu.addClass('go-down');
+            this._$menu.css('bottom', -menuHeight);
+        }
+
+    },
+
+    _hideMenu: function () {
+        this._$menu.addClass('hidden');
+    },
+
+    // инициализация, добавляем подписчика на щелчок по нашей кнопке
+    init: function () {
         this._$switchButton.on('click', this._onSwitchButtonClick.bind(this));
     }
 }
 
 
+// хватаемся за кнопку
 var switchBTN = $('button[data-role="show-menu"]');
+
+// создаем инстанс виджета
 var contextMenu = new ContextMenu(switchBTN);
+// инициализируем виджет
 contextMenu.init()
 
 
